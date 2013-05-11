b9ecea79509302e6473ad033b246daaafb9e34d9
Bing W <iceless.bing@gmail.com>
2013-05-11 20:16:21 +0800

    Initial commit. -street

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Street::Application.config.secret_token = '1a69cfe4ea9561d4b56709af43a7b71124597f3e66c92e29bcc7517279d15b31d5f2c4396d6c0667d76d553efcf13d3ff31c4ba61af50479efb5cce742408d4b'
